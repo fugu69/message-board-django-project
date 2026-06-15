c13e4a7f17c7189c6607a9c31c8974617ab1628b
Maximus Brutalis <primeskobelev@gmail.com>
2026-06-15 15:55:36 +0700

    refactor(posts): migrate from FBV to CBV ListView for posts

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from .models import Post
+
+""" Function-Based View
 
 def post_list(request):
     posts = Post.objects.all()
 
     return render(request, 'post_list.html', context={'posts':posts})
+
+"""
+
+# ListView
+
+class PostList(ListView):
+    model = Post
+    template_name = 'post_list.html'
